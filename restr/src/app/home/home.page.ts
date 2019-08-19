c03ca0305293a51df57643b496d6f08b2b111df6
Owen Cedric Crowie <Owen.Crowie@eoh.com>
2019-08-19 18:12:03 +0200

    Completed App

--- restr/src/app/home/home.page.ts
+++ restr/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  //constructor() {}
 
   percent: number = 0;
   radius:number = 100;
@@ -26,6 +27,10 @@ export class HomePage {
 
   overallTimer: any = false;
 
+constructor(private insomnia: Insomnia) {
+
+}
+
   startTime() {
 
     if(this.timer) {
@@ -34,6 +39,7 @@ export class HomePage {
 
     if(!this.overallTimer) {
       this.progressTimer();
+      this.insomnia.keepAwake();
     }
 
     this.timer = false;
@@ -94,7 +100,7 @@ export class HomePage {
         m:'00',
         s: '00'
       }
-      //tis.insomnia.allowSleepAgain()
+      this.insomnia.allowSleepAgain()
   }
 
 }
